1543960eb65be1f6f36461ce95366436a367c161
Jehú Elías Aravena Retamales <jeh.aravena@duocuc.cl>
2023-08-16 20:45:42 -0400

    Commit inicial

---
+++ steller/public_html/assets/js/steller.js
@@ -0,0 +1,59 @@
+/*!
+=========================================================
+* Steller Landing page
+=========================================================
+
+* Copyright: 2019 DevCRUD (https://devcrud.com)
+* Licensed: (https://devcrud.com/licenses)
+* Coded by www.devcrud.com
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
+
+// smooth scroll
+$(document).ready(function () {
+  $(".nav-link").on("click", function (event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+
+      var hash = this.hash;
+
+      $("html, body").animate(
+        {
+          scrollTop: $(hash).offset().top,
+        },
+        700,
+        function () {
+          window.location.hash = hash;
+        }
+      );
+    }
+  });
+});
+
+$(document).ready(function () {
+  $(".animar").on("click", function (event) {
+    event.preventDefault();
+
+    var hash = "#contact";
+
+    $("html, body").animate(
+      {
+        scrollTop: $(hash).offset().top,
+      },
+      700,
+      function () {
+        window.location.hash = hash;
+      }
+    );
+  });
+});
+
+$(document).ready(function () {
+  $(".descargar").on("click", function () {
+    $("#descarga").attr("href", "../../mis imagenes/jehuaravena_github.pdf");
+    $("#descarga")[0].click();
+  });
+});
